268938d514f87636e09dc108ef5b35f380af6fc6
Bonatrix <beatrizbonates1984@gmail.com>
2025-05-15 17:07:26 -0400

    Atualizacão Enemy script

--- medieval-warrior/ENEMIES/Comportamentos/seguir.gd
+++ medieval-warrior/ENEMIES/Comportamentos/seguir.gd
@@ -1,23 +1,34 @@
-extends CharacterBody2D
+extends Node
 
-@export var speed = 1.0
+@export var speed = 1
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+var enemy: Enemy
+var sprite: AnimatedSprite2D
+
+
+func _read():
+	enemy = get_parent()
+	sprite = enemy.get_node("AnimatedSprite2D")
+	enemy.health
+	pass
+	
+	
+	
 func _physics_process(delta: float) -> void:
 	var player_position = GameManager.player_position
-	var diference =player_position - position
+	var diference =  player_position - enemy.position
 	var input_vector = diference.normalized()
 		
 	
-		# Girar o sprite 	
+	#input vector
+	#direção
+	enemy.velocity = input_vector * speed * 100.0
+
+	enemy.move_and_slide()
+	
+			 #Girar o sprite 	
 	if input_vector.x > 0:
 		sprite.flip_h = false
 	elif input_vector.x < 0:		
 		sprite.flip_h = true		
 		
-	#input vector
-	#direção
-	velocity = input_vector * speed * 100.0
-	#velocity = alguma coisa
-	
-	move_and_slide()
